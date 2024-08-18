3d8018c6faa5870c71c05c8970e08c50a94d7e88
master <master@device-38.home>
2024-08-18 18:45:18 +0200

    [ADD] header comportement - hidding top, color rectification on template & button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header: React.FC = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  let lastScrollY = 0;
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      // Vers le bas
+      setIsHidden(true);
+    } else {
+      // Vers le haut
+      setIsHidden(false);
+    }
+    lastScrollY = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="app-header">
+    <header className={`app-header ${isHidden ? 'hidden' : ''}`}>
       <h1>Échelle de Fatigue de Pichot</h1>
     </header>
   );
--- src/components/IntroModal.tsx
+++ src/components/IntroModal.tsx
@@ -13,8 +13,7 @@ const IntroModal: React.FC<IntroModalProps> = ({ isOpen, onClose }) => {
                 Application développée sous la direction de <strong>DE CLERCK Catherine</strong> dans le cadre de l'obtention de son diplôme UC2 CC Plongée Profonde et Tutorat.
             </p>
             <p><i>
-                L'échelle de Pichot est un outil couramment utilisé pour évaluer la fatigue chez les individus.
-                Elle est composée de huit items simples que les patients doivent évaluer en fonction de leur expérience récente.
+                L'échelle de Pichot est un outil couramment utilisé pour évaluer la fatigue chez les individus.<br />
                 Elle permet de quantifier la fatigue ressentie sur une courte période, généralement les sept derniers jours.
             </i></p>
             <h3>Structure de l’échelle :</h3>
@@ -22,10 +21,10 @@ const IntroModal: React.FC<IntroModalProps> = ({ isOpen, onClose }) => {
                 Les patients sont invités à répondre à chaque item sur une échelle de 0 à 4, où 0 signifie "pas du tout" et 4 signifie "extrêmement".
             </i></p>
             <h3>Interprétation des Résultats :</h3>
-            <p><i>0 - 8 : Pas de fatigue significative</i></p>
-            <p><i>9 - 16 : Fatigue légère</i></p>
-            <p><i>17 - 24 : Fatigue modérée</i></p>
-            <p><i>25 - 32 : Fatigue sévère</i></p>
+            <p><i>0 - 8 : Pas de fatigue significative<br />
+            9 - 16 : Fatigue légère<br />
+            17 - 24 : Fatigue modérée<br />
+            25 - 32 : Fatigue sévère</i></p>
             <button onClick={onClose} className="modal-button">
                 Fermer
             </button>
